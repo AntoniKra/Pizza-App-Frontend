fe580aaace086243f82cb429cc926c9bb910b1cf
Antoni Krawczyk <reio.spoko@outlook.com>
2026-01-24 00:09:36 +0100

    Konfiguracja Orval i wygenerowanie klienta API

---
+++ orval.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  pizzaRadar: {
+    output: {
+      mode: "tags-split",
+      target: "src/api/endpoints",
+      schemas: "src/api/model",
+      client: "axios",
+      mock: true,
+      prettier: true,
+    },
+    input: {
+      // 1. Tutaj wklejamy link od kolegi
+      target: "https://localhost:7115/openapi/v1.json",
+    },
+  },
+});
