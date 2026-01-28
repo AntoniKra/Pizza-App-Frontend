09301b52fb2c6112bfbdd0ba001693b4d6af9cac
Katarzyna Lisiecka <lisiecka.dev@gmail.com>
2026-01-28 21:08:46 +0100

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios"; // <--- 1. Importujemy axios
 import { AuthProvider } from "./context/AuthContext";
 
-// 2. Ustawiamy adres backendu (taki sam jak w orval.config.ts)
-axios.defaults.baseURL = "https://localhost:7115";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+axios.defaults.baseURL = apiUrl;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
